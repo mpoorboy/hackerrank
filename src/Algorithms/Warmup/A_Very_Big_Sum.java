5af426dcbf67a302b87b1c01407bf0bc3c06dd91
Marisa Poorboy <mpoorboy@users.noreply.github.com>
2016-06-08 17:13:00 -0700

    Create A_Very_Big_Sum.java

---
+++ src/Algorithms/Warmup/A_Very_Big_Sum.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class A_Very_Big_Sum {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+	    int T = sc.nextInt();
+        long sum = 0;
+        
+        for (int i = 0; i < T; i++) {
+            long N = sc.nextLong();
+            sum += N;
+        }
+        
+        System.out.println(sum);
+    }
+}
