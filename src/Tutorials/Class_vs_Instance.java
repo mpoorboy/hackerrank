165fa0325f695c061ecb0eb883b55328d78db9d0
Marisa Poorboy <mpoorboy@users.noreply.github.com>
2016-06-08 17:06:15 -0700

    Update Class_vs_Instance.java

--- src/Tutorials/Class_vs_Instance.java
+++ src/Tutorials/Class_vs_Instance.java
@@ -4,29 +4,28 @@ import java.util.*;
 public class Person {
     private int age;	
   
-	public Person(int initialAge) {
+  public Person(int initialAge) {
     this.age = initialAge;
-    }
+  }
 
-	public void amIOld() {
-        	if (age <= 0) {
-         System.out.println("Age is not valid, setting age to 0.");
-                System.out.println("You are young.");
-                this.age = 0;
-     }
-  	else if (age >= 18) {
-         System.out.println("You are old.");
-     } else if (age >= 13) {
-         System.out.println("You are a teenager.");
-     } else {
+  public void amIOld() {
+    if (age <= 0) {
+      System.out.println("Age is not valid, setting age to 0.");
+      System.out.println("You are young.");
+      this.age = 0;
+    } else if (age >= 18) {
+        System.out.println("You are old.");
+      } else if (age >= 13) {
+        System.out.println("You are a teenager.");
+      } else {
         System.out.println("You are young.");
-    }
+      }
         
-	}
+   }
 
-	public void yearPasses() {
-  	this.age++;
-	}
+   public void yearPasses() {
+    this.age++;
+   }
 	//End code
 public static void main(String[] args) {
 	Scanner sc = new Scanner(System.in);
