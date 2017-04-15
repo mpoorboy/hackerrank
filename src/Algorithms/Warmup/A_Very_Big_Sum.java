5775529f057209e226154f176148e5f6d1e4c354
Marisa Poorboy <mjpoorboy@gmail.com>
2017-04-14 17:46:53 -0700

    Update A_Very_Big_Sum.java

--- src/Algorithms/Warmup/A_Very_Big_Sum.java
+++ src/Algorithms/Warmup/A_Very_Big_Sum.java
@@ -5,12 +5,12 @@ public class A_Very_Big_Sum {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-	    int T = sc.nextInt();
-        long sum = 0;
+	 int T = sc.nextInt();
+         long sum = 0;
         
         for (int i = 0; i < T; i++) {
-            long N = sc.nextLong();
-            sum += N;
+         long N = sc.nextLong();
+         sum += N;
         }
         
         System.out.println(sum);
